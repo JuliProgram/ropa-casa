7ea6d890a4ef2937ee4e43d658372587530b0669
Juliprogram <pelukman47155796@gmail.com>
2024-04-25 17:54:26 -0300

    ultima subida

--- index.php
+++ index.php
@@ -26,12 +26,24 @@
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="images/invierno photoshoppng.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+    <h1>Descuentos!</h1>
+    <p>Aprovecha y ahorra en las siguientes prendas</p>
+    </div>
     </div>
     <div class="carousel-item">
       <img src="images/inviernopho2.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+    <h1>Descuentos!</h1>
+    <p>Aprovecha y ahorra en las siguientes prendas</p>
+    </div>
     </div>
     <div class="carousel-item">
       <img src="images/hombre-invierno.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+    <h1>Descuentos!</h1>
+    <p>Aprovecha y ahorra en las siguientes prendas</p>
+    </div>
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
@@ -44,11 +56,69 @@
   </button>
 </div>
       </div>
-      <div class="row">
-        <div class="col-6">asdfkdsaf</div>
-        <div class="col-6">asdfkdsaf</div>
-      </div>
     </main>
+    <section>
+        <div class="row">
+          <div class="col-12 mt-4 separacion">
+            <div class="container">
+              <div class="row">
+                <div class="col-3 fondoInterno">
+                    <img class="img-item" src="images/invierno1.png" alt=""> 
+                    <div class="container">
+            <div class="row">
+                <div class="col-3 fondoInterno mx-2">
+                <div class="promotion-item__today-offer-container">
+                  <span class="promotion-item__today-offer-text">OFERTA</span>
+                </div>
+                <s class="andes-money-amount andes-money-amount-combo__previous-value andes-money-amount--previous andes-money-amount--cents-comma" style="font-size:12px" role="img" aria-label="Antes: 1739977 pesos" aria-roledescription="Precio"><span class="andes-money-amount__currency-symbol" aria-hidden="true">$</span><span class="andes-money-amount__fraction" aria-hidden="true">41.500</span></s>
+                <div class="andes-money-amount-combo__main-container"><span class="andes-money-amount andes-money-amount--cents-superscript" style="font-size:24px" role="img" aria-label="Ahora: 361115 pesos" aria-roledescription="Precio"><span class="andes-money-amount__currency-symbol" aria-hidden="true">$</span><span class="andes-money-amount__fraction" aria-hidden="true">36.500</span></span>
+                <span class="descuento">20% OFF</span>
+                <p class="promotion-item__title">Abrigo invierno</p>
+              </div>
+              </div>   
+            </div>
+          </div>
+                </div>
+                <div class="col-3 fondoInterno mx-2">
+                    <img class="img-item" src="images/invierno1.png" alt=""> 
+                    <div class="container">
+            <div class="row">
+                <div class="col-3 fondoInterno">
+                <div class="promotion-item__today-offer-container">
+                  <span class="promotion-item__today-offer-text">OFERTA</span>
+                </div>
+                <s class="andes-money-amount andes-money-amount-combo__previous-value andes-money-amount--previous andes-money-amount--cents-comma" style="font-size:12px" role="img" aria-label="Antes: 1739977 pesos" aria-roledescription="Precio"><span class="andes-money-amount__currency-symbol" aria-hidden="true">$</span><span class="andes-money-amount__fraction" aria-hidden="true">41.500</span></s>
+                <div class="andes-money-amount-combo__main-container"><span class="andes-money-amount andes-money-amount--cents-superscript" style="font-size:24px" role="img" aria-label="Ahora: 361115 pesos" aria-roledescription="Precio"><span class="andes-money-amount__currency-symbol" aria-hidden="true">$</span><span class="andes-money-amount__fraction" aria-hidden="true">36.500</span></span>
+                <span class="descuento">20% OFF</span>
+                <p class="promotion-item__title">Abrigo invierno</p>
+              </div>
+              </div>   
+            </div>
+              </div>
+                </div>
+                <div class="col-3 fondoInterno mx-1">
+                    <img class="img-item" src="images/invierno1.png" alt=""> 
+                    <div class="container">
+            <div class="row">
+                <div class="col-3 fondoInterno">
+                <div class="promotion-item__today-offer-container">
+                  <span class="promotion-item__today-offer-text">OFERTA</span>
+                </div>
+                <s class="andes-money-amount andes-money-amount-combo__previous-value andes-money-amount--previous andes-money-amount--cents-comma" style="font-size:12px" role="img" aria-label="Antes: 1739977 pesos" aria-roledescription="Precio"><span class="andes-money-amount__currency-symbol" aria-hidden="true">$</span><span class="andes-money-amount__fraction" aria-hidden="true">41.500</span></s>
+                <div class="andes-money-amount-combo__main-container"><span class="andes-money-amount andes-money-amount--cents-superscript" style="font-size:24px" role="img" aria-label="Ahora: 361115 pesos" aria-roledescription="Precio"><span class="andes-money-amount__currency-symbol" aria-hidden="true">$</span><span class="andes-money-amount__fraction" aria-hidden="true">36.500</span></span>
+                <span class="descuento">20% OFF</span>
+                <p class="promotion-item__title">Abrigo invierno</p>
+              </div>
+              </div>   
+            </div>
+              </div>
+                </div>
+              </div>
+            </div>
+        </div>
+        
+        </div>
+      </section>
     <footer class=" bg-danger mt-auto py-1">
       <?php include_once("footer.php") ?>
   </footer>       
