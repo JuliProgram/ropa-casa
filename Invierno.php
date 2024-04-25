66012ef678b2867a1b76c611a6efa73b4dc61838
Juliprogram <pelukman47155796@gmail.com>
2024-04-25 18:42:18 -0300

    ultima version

--- Invierno.php
+++ Invierno.php
@@ -20,29 +20,87 @@
     <header>
         <?php include_once("header.php") ?>
     </header>
-    <main class="container">
+    <main>
         <section class="mt-5">
-        <div class="container">
-                <!-- Portfolio Grid Items-->
-                <div class="row justify-content-center">
-                    <!-- Portfolio Item 1-->
-                    <div class="col-md-6 col-lg-4 mb-5">
-                        <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
-                            <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
-                            </div>
-                            <img class="invierno-foto" src="images/invierno1.png" alt="">
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-lg-4 mb-5">
-                        <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
-                            <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
-                            </div>
-                            <img class="invierno-foto" src="images/invierno1.png" alt="">
-                        </div>
-                    </div>
+        <div class="row">
+            
+          <div class="col-12 mt-4 separacion">
+            <div class="container">
+                <div>
+                    <h1>Invierno</h1>
+                </div>
+              <div class="row">
+                <div class="col-3 fondoInterno">
+                    <img class="img-item" src="images/invierno1.png" alt=""> 
+                    <div class="container">
+            <div class="row">
+                <div class="col-3 fondoInternoDescripcion ">
+                <div class="promotion-item__today-offer-container">
+                  <span class="promotion-item__today-offer-text">OFERTA</span>
+                </div>
+                <s class="andes-money-amount andes-money-amount-combo__previous-value andes-money-amount--previous andes-money-amount--cents-comma" style="font-size:12px" role="img" aria-label="Antes: 1739977 pesos" aria-roledescription="Precio"><span class="andes-money-amount__currency-symbol" aria-hidden="true">$</span><span class="andes-money-amount__fraction" aria-hidden="true">41.500</span></s>
+                <div class="andes-money-amount-combo__main-container"><span class="andes-money-amount andes-money-amount--cents-superscript" style="font-size:24px" role="img" aria-label="Ahora: 361115 pesos" aria-roledescription="Precio"><span class="andes-money-amount__currency-symbol" aria-hidden="true">$</span><span class="andes-money-amount__fraction" aria-hidden="true">36.500</span></span>
+                <span class="descuento">20% OFF</span>
+                <p class="promotion-item__title">Abrigo invierno</p>
+              </div>
+              </div>   
             </div>
+          </div>
+                </div>
+                <div class="col-3 fondoInterno ">
+                    <img class="img-item" src="images/invierno1.png" alt=""> 
+                    <div class="container">
+            <div class="row">
+                <div class="col-3 fondoInternoDescripcion">
+                <div class="promotion-item__today-offer-container">
+                  <span class="promotion-item__today-offer-text">OFERTA</span>
+                </div>
+                <s class="andes-money-amount andes-money-amount-combo__previous-value andes-money-amount--previous andes-money-amount--cents-comma" style="font-size:12px" role="img" aria-label="Antes: 1739977 pesos" aria-roledescription="Precio"><span class="andes-money-amount__currency-symbol" aria-hidden="true">$</span><span class="andes-money-amount__fraction" aria-hidden="true">41.500</span></s>
+                <div class="andes-money-amount-combo__main-container"><span class="andes-money-amount andes-money-amount--cents-superscript" style="font-size:24px" role="img" aria-label="Ahora: 361115 pesos" aria-roledescription="Precio"><span class="andes-money-amount__currency-symbol" aria-hidden="true">$</span><span class="andes-money-amount__fraction" aria-hidden="true">36.500</span></span>
+                <span class="descuento">20% OFF</span>
+                <p class="promotion-item__title">Abrigo invierno</p>
+              </div>
+              </div>   
+            </div>
+              </div>
+                </div>
+                <div class="col-3 fondoInterno">
+                    <img class="img-item" src="images/invierno1.png" alt=""> 
+                    <div class="container">
+            <div class="row">
+                <div class="col-3 fondoInternoDescripcion">
+                <div class="promotion-item__today-offer-container">
+                  <span class="promotion-item__today-offer-text">OFERTA</span>
+                </div>
+                <s class="andes-money-amount andes-money-amount-combo__previous-value andes-money-amount--previous andes-money-amount--cents-comma" style="font-size:12px" role="img" aria-label="Antes: 1739977 pesos" aria-roledescription="Precio"><span class="andes-money-amount__currency-symbol" aria-hidden="true">$</span><span class="andes-money-amount__fraction" aria-hidden="true">41.500</span></s>
+                <div class="andes-money-amount-combo__main-container"><span class="andes-money-amount andes-money-amount--cents-superscript" style="font-size:24px" role="img" aria-label="Ahora: 361115 pesos" aria-roledescription="Precio"><span class="andes-money-amount__currency-symbol" aria-hidden="true">$</span><span class="andes-money-amount__fraction" aria-hidden="true">36.500</span></span>
+                <span class="descuento">20% OFF</span>
+                <p class="promotion-item__title">Abrigo invierno</p>
+              </div>
+              </div>   
+            </div>
+              </div>
+                </div>
+                <div class="col-3 fondoInterno ">
+                    <img class="img-item" src="images/invierno1.png" alt=""> 
+                    <div class="container">
+            <div class="row">
+                <div class="col-3 fondoInternoDescripcion">
+                <div class="promotion-item__today-offer-container">
+                  <span class="promotion-item__today-offer-text">OFERTA</span>
+                </div>
+                <s class="andes-money-amount andes-money-amount-combo__previous-value andes-money-amount--previous andes-money-amount--cents-comma" style="font-size:12px" role="img" aria-label="Antes: 1739977 pesos" aria-roledescription="Precio"><span class="andes-money-amount__currency-symbol" aria-hidden="true">$</span><span class="andes-money-amount__fraction" aria-hidden="true">41.500</span></s>
+                <div class="andes-money-amount-combo__main-container"><span class="andes-money-amount andes-money-amount--cents-superscript" style="font-size:24px" role="img" aria-label="Ahora: 361115 pesos" aria-roledescription="Precio"><span class="andes-money-amount__currency-symbol" aria-hidden="true">$</span><span class="andes-money-amount__fraction" aria-hidden="true">36.500</span></span>
+                <span class="descuento">20% OFF</span>
+                <p class="promotion-item__title">Abrigo invierno</p>
+              </div>
+              </div>   
+            </div>
+              </div>
+                </div>
+              </div>
+            </div>
+        </div>
         </div>
         </section>
     </main>
--- index.php
+++ index.php
@@ -58,6 +58,7 @@
       </div>
     </main>
     <section>
+    <!-- Primera Fila -->
         <div class="row">
           <div class="col-12 mt-4 separacion">
             <div class="container">
@@ -66,7 +67,7 @@
                     <img class="img-item" src="images/invierno1.png" alt=""> 
                     <div class="container">
             <div class="row">
-                <div class="col-3 fondoInterno mx-2">
+                <div class="col-3 fondoInternoDescripcion ">
                 <div class="promotion-item__today-offer-container">
                   <span class="promotion-item__today-offer-text">OFERTA</span>
                 </div>
@@ -79,11 +80,11 @@
             </div>
           </div>
                 </div>
-                <div class="col-3 fondoInterno mx-2">
+                <div class="col-3 fondoInterno ">
                     <img class="img-item" src="images/invierno1.png" alt=""> 
                     <div class="container">
             <div class="row">
-                <div class="col-3 fondoInterno">
+                <div class="col-3 fondoInternoDescripcion">
                 <div class="promotion-item__today-offer-container">
                   <span class="promotion-item__today-offer-text">OFERTA</span>
                 </div>
@@ -96,11 +97,28 @@
             </div>
               </div>
                 </div>
-                <div class="col-3 fondoInterno mx-1">
+                <div class="col-3 fondoInterno">
                     <img class="img-item" src="images/invierno1.png" alt=""> 
                     <div class="container">
             <div class="row">
-                <div class="col-3 fondoInterno">
+                <div class="col-3 fondoInternoDescripcion">
+                <div class="promotion-item__today-offer-container">
+                  <span class="promotion-item__today-offer-text">OFERTA</span>
+                </div>
+                <s class="andes-money-amount andes-money-amount-combo__previous-value andes-money-amount--previous andes-money-amount--cents-comma" style="font-size:12px" role="img" aria-label="Antes: 1739977 pesos" aria-roledescription="Precio"><span class="andes-money-amount__currency-symbol" aria-hidden="true">$</span><span class="andes-money-amount__fraction" aria-hidden="true">41.500</span></s>
+                <div class="andes-money-amount-combo__main-container"><span class="andes-money-amount andes-money-amount--cents-superscript" style="font-size:24px" role="img" aria-label="Ahora: 361115 pesos" aria-roledescription="Precio"><span class="andes-money-amount__currency-symbol" aria-hidden="true">$</span><span class="andes-money-amount__fraction" aria-hidden="true">36.500</span></span>
+                <span class="descuento">20% OFF</span>
+                <p class="promotion-item__title">Abrigo invierno</p>
+              </div>
+              </div>   
+            </div>
+              </div>
+                </div>
+                <div class="col-3 fondoInterno ">
+                    <img class="img-item" src="images/invierno1.png" alt=""> 
+                    <div class="container">
+            <div class="row">
+                <div class="col-3 fondoInternoDescripcion">
                 <div class="promotion-item__today-offer-container">
                   <span class="promotion-item__today-offer-text">OFERTA</span>
                 </div>
@@ -116,7 +134,83 @@
               </div>
             </div>
         </div>
-        
+        </div>
+    <!-- Secunda Fila -->
+    <div class="row">
+          <div class="col-12  separacion">
+            <div class="container">
+              <div class="row">
+                <div class="col-3 fondoInterno">
+                    <img class="img-item" src="images/invierno1.png" alt=""> 
+                    <div class="container">
+            <div class="row">
+                <div class="col-3 fondoInternoDescripcion ">
+                <div class="promotion-item__today-offer-container">
+                  <span class="promotion-item__today-offer-text">OFERTA</span>
+                </div>
+                <s class="andes-money-amount andes-money-amount-combo__previous-value andes-money-amount--previous andes-money-amount--cents-comma" style="font-size:12px" role="img" aria-label="Antes: 1739977 pesos" aria-roledescription="Precio"><span class="andes-money-amount__currency-symbol" aria-hidden="true">$</span><span class="andes-money-amount__fraction" aria-hidden="true">41.500</span></s>
+                <div class="andes-money-amount-combo__main-container"><span class="andes-money-amount andes-money-amount--cents-superscript" style="font-size:24px" role="img" aria-label="Ahora: 361115 pesos" aria-roledescription="Precio"><span class="andes-money-amount__currency-symbol" aria-hidden="true">$</span><span class="andes-money-amount__fraction" aria-hidden="true">36.500</span></span>
+                <span class="descuento">20% OFF</span>
+                <p class="promotion-item__title">Abrigo invierno</p>
+              </div>
+              </div>   
+            </div>
+          </div>
+                </div>
+                <div class="col-3 fondoInterno ">
+                    <img class="img-item" src="images/invierno1.png" alt=""> 
+                    <div class="container">
+            <div class="row">
+                <div class="col-3 fondoInternoDescripcion">
+                <div class="promotion-item__today-offer-container">
+                  <span class="promotion-item__today-offer-text">OFERTA</span>
+                </div>
+                <s class="andes-money-amount andes-money-amount-combo__previous-value andes-money-amount--previous andes-money-amount--cents-comma" style="font-size:12px" role="img" aria-label="Antes: 1739977 pesos" aria-roledescription="Precio"><span class="andes-money-amount__currency-symbol" aria-hidden="true">$</span><span class="andes-money-amount__fraction" aria-hidden="true">41.500</span></s>
+                <div class="andes-money-amount-combo__main-container"><span class="andes-money-amount andes-money-amount--cents-superscript" style="font-size:24px" role="img" aria-label="Ahora: 361115 pesos" aria-roledescription="Precio"><span class="andes-money-amount__currency-symbol" aria-hidden="true">$</span><span class="andes-money-amount__fraction" aria-hidden="true">36.500</span></span>
+                <span class="descuento">20% OFF</span>
+                <p class="promotion-item__title">Abrigo invierno</p>
+              </div>
+              </div>   
+            </div>
+              </div>
+                </div>
+                <div class="col-3 fondoInterno">
+                    <img class="img-item" src="images/invierno1.png" alt=""> 
+                    <div class="container">
+            <div class="row">
+                <div class="col-3 fondoInternoDescripcion">
+                <div class="promotion-item__today-offer-container">
+                  <span class="promotion-item__today-offer-text">OFERTA</span>
+                </div>
+                <s class="andes-money-amount andes-money-amount-combo__previous-value andes-money-amount--previous andes-money-amount--cents-comma" style="font-size:12px" role="img" aria-label="Antes: 1739977 pesos" aria-roledescription="Precio"><span class="andes-money-amount__currency-symbol" aria-hidden="true">$</span><span class="andes-money-amount__fraction" aria-hidden="true">41.500</span></s>
+                <div class="andes-money-amount-combo__main-container"><span class="andes-money-amount andes-money-amount--cents-superscript" style="font-size:24px" role="img" aria-label="Ahora: 361115 pesos" aria-roledescription="Precio"><span class="andes-money-amount__currency-symbol" aria-hidden="true">$</span><span class="andes-money-amount__fraction" aria-hidden="true">36.500</span></span>
+                <span class="descuento">20% OFF</span>
+                <p class="promotion-item__title">Abrigo invierno</p>
+              </div>
+              </div>   
+            </div>
+              </div>
+                </div>
+                <div class="col-3 fondoInterno ">
+                    <img class="img-item" src="images/invierno1.png" alt=""> 
+                    <div class="container">
+            <div class="row">
+                <div class="col-3 fondoInternoDescripcion">
+                <div class="promotion-item__today-offer-container">
+                  <span class="promotion-item__today-offer-text">OFERTA</span>
+                </div>
+                <s class="andes-money-amount andes-money-amount-combo__previous-value andes-money-amount--previous andes-money-amount--cents-comma" style="font-size:12px" role="img" aria-label="Antes: 1739977 pesos" aria-roledescription="Precio"><span class="andes-money-amount__currency-symbol" aria-hidden="true">$</span><span class="andes-money-amount__fraction" aria-hidden="true">41.500</span></s>
+                <div class="andes-money-amount-combo__main-container"><span class="andes-money-amount andes-money-amount--cents-superscript" style="font-size:24px" role="img" aria-label="Ahora: 361115 pesos" aria-roledescription="Precio"><span class="andes-money-amount__currency-symbol" aria-hidden="true">$</span><span class="andes-money-amount__fraction" aria-hidden="true">36.500</span></span>
+                <span class="descuento">20% OFF</span>
+                <p class="promotion-item__title">Abrigo invierno</p>
+              </div>
+              </div>   
+            </div>
+              </div>
+                </div>
+              </div>
+            </div>
+        </div>
         </div>
       </section>
     <footer class=" bg-danger mt-auto py-1">
